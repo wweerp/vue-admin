7db47922f8778b5dcf87e471ef582a62da2aa2b1
wweerp <18352868150@163.com>
2021-01-07 18:58:08 +0800

    登录·注册按钮的编写

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,17 +1,58 @@
 <template>
   <div id="login">
-    在这里是首页
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <div class="login-warp">
+      <ul class="menu-tab">
+        <li v-for="item in menuTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item)">{{item.txt}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data(){
+    return {
+      menuTab:[
+        {txt: '登录',current:true},
+        {txt: '注册',current:false}
+      ],
+      isActive:true
+    }
+  },
+  methods:{
+    toggleMenu(data){
+      //ES6的for循环menuTab
+      this.menuTab.forEach(elem => {
+        elem.current = false
+      });
+      //高光效果
+       data.current = true
+    }
+  }
 };
 </script>
-<style scoped></style>
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+.login-warp {
+   width: 330px;
+   margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width:88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+}
+.current {
+  background-color: rgba(0,0,0,.1);
+}
+</style>
